7f3c2407bbe514898b79f9638ba71a5f5630a4f1
benfo <ben.foxcroft@gmail.com>
2023-04-06 11:54:20 +0100

    removing raw pointer use in LWindow.h (and subsequently main)

--- 10_colour_keying/LWindow.h
+++ 10_colour_keying/LWindow.h
@@ -18,7 +18,7 @@ public:
 
     void close(LTexture& backgroundTexture, LTexture& fooTexture);
 
-    SDL_Renderer* get(){ return m_Renderer.get(); }
+    SDL_Renderer get(){ return  *m_Renderer; }
 
     friend class LTexture;
     
